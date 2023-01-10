4b76a470ba41fc082387203a7a0da947e8fc3116
Afganli97 <ulviva@code.edu.az>
2023-01-11 01:47:42 +0400

    Make slider's photos dynamic

--- FrontToBack/Controllers/HomeController.cs
+++ FrontToBack/Controllers/HomeController.cs
@@ -1,12 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FrontToBack.DAL;
+using FrontToBack.Models;
+using FrontToBack.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace FrontToBack.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataBase _context;
+
+        public HomeController(DataBase context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM();
+            homeVM.Sliders = _context.Sliders.ToList();
+
+            return View(homeVM);
         }
     }
 }
---
+++ FrontToBack/ViewModels/HomeVM.cs
@@ -0,0 +1,10 @@
+﻿using FrontToBack.Models;
+using System.Collections.Generic;
+
+namespace FrontToBack.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Slider> Sliders { get; set; }
+    }
+}
